b9298cbd351a5248747455ee9f62cc21789baad5
Timofey Martynov <mart-tima@yandex.ru>
2019-10-11 18:39:47 +0500

    Text parse completed

--- GeneratorApp/backend/parser.py
+++ GeneratorApp/backend/parser.py
@@ -1,23 +1,36 @@
-import json, requests
+import json, requests, time
 from bs4 import BeautifulSoup
+from entity.diplom import Diplom
+
+wordsCount = 0
+firstIteration = 0
 
 # topics - МАССИВ тем по диплому (не кидать строку, даже если элемент один)
-def getTextOnTopic(topics):
+def getTextOnTopic(topics, countWords):
+  global wordsCount
+  global firstIteration
+  firstIteration = 0
+  wordsCount = 0
+  diplom = Diplom()
+
   topic = '+'.join(topics)
-  response = requests.get('http://yandex.ru/referats/?t='+topic)
-  text = parseText(response.text)
-  print(text)
+  while wordsCount < countWords:
+    response = requests.get('http://yandex.ru/referats/?t='+topic)
+    parseText(response.text, diplom)
+  
+  return diplom
 
-# def getText
+def parseText(html, diplom):
+  global firstIteration
+  global wordsCount
 
-def parseText(html):
   soup = BeautifulSoup(html, 'lxml')
-  paragraphs = []
   text = soup.find('div', class_='referats__text')
-  topic = text.find('strong').text
+  if not firstIteration:
+    diplom.topic = text.find('strong').text
+    firstIteration = 1
+  
   paragraph = text.find_all('p')
   for i in paragraph:
-    paragraphs.append(i.text)
-  
-  print(paragraphs)
-  return json.dumps({'topic': topic, 'paragraphs': paragraphs})
+    diplom.paragraphs.append(i.text)
+    wordsCount += len(i.text.split())
