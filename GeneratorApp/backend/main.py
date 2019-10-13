21c657e75387ff862c08fe9e13c7dd0763e253df
Kostya Bragin <bragkostiy99@gmail.com>
2019-10-13 15:48:18 +0500

    POST request and send data to Parser

--- GeneratorApp/backend/main.py
+++ GeneratorApp/backend/main.py
@@ -7,7 +7,12 @@ template_dir = os.path.abspath('..')
 app = Flask(__name__, template_folder=template_dir)
 CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+  if request.method == 'POST':
+    print(request.json['name'])
+    #parser.getTextOnTopic(request.json['Topic'], request.json['CountWord'])
+    return jsonify({'message': "данные получены!"})
   #parser.getTextOnTopic(['physics', 'law'])
   return render_template('index.html')
+
